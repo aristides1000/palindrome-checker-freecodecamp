294515856a3e5c7a2426c6ab24360e30ff2a7981
aristides1000 <aristides1000@gmail.com>
2025-07-22 16:09:19 +0000

    Feat: Finish task 5

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ const checkBtn = document.querySelector('#check-btn');
 const result = document.querySelector('#result');
 
 checkBtn.addEventListener("click", () => {
-  if (textInput.textContent === '') {
+  if (textInput.value.length === 0) {
     alert('Please input a value');
+  }
+
+  if (textInput.value.length === 1) {
+    result.textContent = `${textInput.value} is a palindrome`;
   }
 });
